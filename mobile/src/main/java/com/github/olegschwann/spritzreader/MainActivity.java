17f151f91b71f11d024815e3797d12e84996a4a1
Ryazanov Maxim <ryazanovms@yandex.ru>
2019-04-26 00:36:57 +0300

    feat: Include auth_sdk

--- mobile/src/main/java/com/github/olegschwann/spritzreader/MainActivity.java
+++ mobile/src/main/java/com/github/olegschwann/spritzreader/MainActivity.java
@@ -1,7 +1,20 @@
 package com.github.olegschwann.spritzreader;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import ru.mail.auth.sdk.AuthError;
+import ru.mail.auth.sdk.AuthResult;
+import ru.mail.auth.sdk.MailRuAuthSdk;
+import ru.mail.auth.sdk.MailRuCallback;
+import ru.mail.auth.sdk.ui.MailRuLoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MailRuAuthSdk.initialize(getApplicationContext());
+        Button loginBtn = findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MailRuAuthSdk.getInstance().startLogin(MainActivity.this);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(!MailRuAuthSdk.getInstance().handleActivityResult(requestCode, resultCode, data, new MailRuCallback<AuthResult, AuthError>() {
+            @Override
+            public void onResult(AuthResult result) {
+                Toast.makeText(MainActivity.this, "Success " + result.getAuthCode(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(AuthError error) {
+                Toast.makeText(MainActivity.this, error.getErrorReason() + " " + error.name(), Toast.LENGTH_LONG).show();
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
